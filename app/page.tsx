c0fa1c66998d80b575bc40c99317ea28caf40c9d
Collin Morrison <collinam@byu.edu>
2024-04-19 16:33:38 -0600

    experimenting with different navbar locations

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,18 @@ import { toast } from "react-toastify";
 
 
 export default function Home() {
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-
 
     return (
         <>
-            <Link href="/register" className="text-white">Register</Link>
+            <div className="w-full h-16 shadow-lg">
+                <div className="h-full mr-10 ml-10 flex justify-end items-center">
+                    <Link href="/login" className="text-white mr-10">Login</Link>
+                    <Link href="/register" className="text-white">Register</Link>
+                </div>
+            </div>
+            <div className="flex justify-center items-center h-full">
+                <h1 className="text-3xl text-white">Welcome to Coachify!</h1>
+            </div>
         </>
     )
 }
